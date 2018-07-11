8e727e1abe008a6b04fc85a8712736c9eca0406f
PatrickKvartsaniy <kvartsaniy@gmail.com>
2018-07-11 16:52:10 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    host, port = '0.0.0.0', int(os.environ.get('PORT', 5000))
+    app.run(host=host, port=port)
